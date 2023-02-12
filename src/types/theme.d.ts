5792588570b20736403f43523086d8e614e2a699
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-02-12 20:11:21 +0200

    Removed unused `h4Primary` variant definition

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,25 +1,4 @@
-import { CSSProperties } from 'react';
-
 import { Theme as MuiTheme } from '@mui/material/styles';
-
-declare module '@mui/material/styles' {
-  interface TypographyVariants {
-    h4Primary: CSSProperties;
-  }
-
-  // allow configuration using `createTheme`
-  interface TypographyVariantsOptions {
-    h4Primary?: CSSProperties;
-  }
-}
-
-// Update the Typography's variant prop options
-declare module '@mui/material/Typography' {
-  interface TypographyPropsVariantOverrides {
-    h4Primary: true;
-  }
-}
-
 import '@emotion/react';
 
 declare module '@emotion/react' {
