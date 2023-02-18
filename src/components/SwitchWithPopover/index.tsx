d19336907c6f5801c5301ad51d9731984041576e
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-02-18 17:12:28 +0200

    Simplified onChange signature

--- src/components/SwitchWithPopover/index.tsx
+++ src/components/SwitchWithPopover/index.tsx
@@ -8,17 +8,19 @@ import {
 } from 'material-ui-popup-state/hooks';
 import HoverPopover from 'material-ui-popup-state/HoverPopover';
 
-interface Props extends SwitchProps {
+interface Props extends Omit<SwitchProps, 'onChange'> {
   helperText: string;
+  onChange: (enabled: boolean) => void;
 }
 
-const SwitchWithPopover: FC<Props> = ({ helperText, ...props }) => {
-  const popoverState = usePopupState({ variant: 'popover' });
+const SwitchWithPopover: FC<Props> = (props) => {
+  const { helperText, onChange, ...switchProps } = props;
 
+  const popoverState = usePopupState({ variant: 'popover' });
   return (
     <>
       <Box {...bindHover(popoverState)}>
-        <Switch {...props} />
+        <Switch {...switchProps} onChange={(e) => onChange(e.target.checked)} />
       </Box>
       <HoverPopover
         {...bindPopover(popoverState)}
