99451b5682263f9e7dd7204796bbbdf130f695c0
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-02-25 16:30:52 +0200

    Added required flag handling for the current user obtaining

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,22 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import { User } from 'next-auth';
 import { SessionContextValue, useSession } from 'next-auth/react';
+import { UseSessionOptions } from 'next-auth/react/types';
 
 import { useGuestUserInfo } from '@/store/guestUserInfo';
 
-export const useCurrentUser = (): {
+export const useCurrentUser = <R extends boolean>(
+  options?: UseSessionOptions<R>,
+): {
   user?: User;
   status: SessionContextValue['status'];
+  isLoading: boolean;
 } => {
+  const [user, setUser] = useState<User>();
+
   const { data: session, status } = useSession();
-
-  if (status === 'loading') {
-    return { status };
-  }
-
-  if (status === 'authenticated') {
-    return { user: session?.user, status };
-  }
+  const isLoading = status === 'loading';
 
   const guest = useGuestUserInfo.use.guest?.();
-  return { user: guest, status };
+
+  useEffect(() => {
+    if (isLoading || user) return;
+
+    const currentUser = session?.user || guest;
+    setUser(currentUser);
+
+    if (options?.required && !currentUser) {
+      options.onUnauthenticated?.();
+    }
+  }, [guest, isLoading, options, session?.user, user]);
+
+  return { user, status, isLoading };
 };
