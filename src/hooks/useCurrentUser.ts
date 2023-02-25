c8912f2c5590153cb3e371987903b2b075b2db7d
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-02-25 20:44:07 +0200

    Added caching for the current user

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -5,21 +5,31 @@ import { SessionContextValue, useSession } from 'next-auth/react';
 
 import { useGuestUserInfo } from '@/store/guestUserInfo';
 
+/**
+ * Reuse the same user object across the components and pages
+ * TODO Use persisted storage to keep the user object.
+ * Update it on the first load of the page and log out if previously was logged
+ */
+let cachedUser: User;
+
 interface Props {
   redirectOnUnauthenticated?: () => void;
 }
 
 export const useCurrentUser = (
-  props: Props,
+  props?: Props,
 ): {
   user?: User;
   status: SessionContextValue['status'];
   isLoading: boolean;
 } => {
-  const { redirectOnUnauthenticated } = props;
+  const { redirectOnUnauthenticated } = {
+    redirectOnUnauthenticated: () => undefined,
+    ...props,
+  };
 
-  const [isLoading, setLoading] = useState(true);
-  const [user, setUser] = useState<User>();
+  const [isLoading, setLoading] = useState(!cachedUser);
+  const [user, setUser] = useState<User>(cachedUser);
 
   const { data: session, status } = useSession();
   const guest = useGuestUserInfo.use.guest?.();
@@ -34,6 +44,7 @@ export const useCurrentUser = (
     const currentUser = session?.user || guest;
     startTransition(() => {
       if (currentUser?.name) {
+        cachedUser = currentUser;
         setUser(currentUser);
         setLoading(false);
       } else {
