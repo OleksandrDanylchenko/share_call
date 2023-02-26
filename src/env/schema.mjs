05a832938c2ce3017d400f2370ef8595fca9fe00
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-02-26 13:38:25 +0200

    Added `NEXT_PUBLIC_AGORA_APP_ID` variable

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -74,7 +74,7 @@ export const serverEnv = {
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_AGORA_APP_ID: z.string(),
 });
 
 /**
@@ -84,5 +84,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_AGORA_APP_ID: process.env.NEXT_PUBLIC_AGORA_APP_ID,
 };
