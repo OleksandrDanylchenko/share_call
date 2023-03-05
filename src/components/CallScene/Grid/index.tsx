03e8a06f3fa2886d769804d5526defafd861e5c0
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-03-05 15:56:11 +0200

    Removed testing items for `Grid`

--- src/components/CallScene/Grid/index.tsx
+++ src/components/CallScene/Grid/index.tsx
@@ -33,13 +33,6 @@ const GridScene: FC = () => {
       alignContent="center"
       justifyContent="center"
     >
-      {/*{items.map((item) => (*/}
-      {/*  <Grid key={item} {...itemDimensions}>*/}
-      {/*    <Item css={fullParent}>*/}
-      {/*      <Typography variant="h5">{item}</Typography>*/}
-      {/*    </Item>*/}
-      {/*  </Grid>*/}
-      {/*))}*/}
       {Object.entries(tracks).map(([userId, { microphone, camera }]) => (
         <Grid key={userId} {...itemDimensions}>
           <TracksPlayer
@@ -56,21 +49,5 @@ const GridScene: FC = () => {
     </Grid>
   );
 };
-
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-//   color: theme.palette.text.secondary,
-// }));
-//
-// const items = [
-//   0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-//   23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-// ];
 
 export default GridScene;
