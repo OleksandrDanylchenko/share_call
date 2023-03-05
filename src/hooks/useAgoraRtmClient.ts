ab78be72a80d722fa2883c9330d55c489a685047
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-03-05 21:10:53 +0200

    Added hook for rtm client retrieval

--- src/hooks/useAgoraRtmClient.ts
+++ src/hooks/useAgoraRtmClient.ts
@@ -1,30 +1,38 @@
 import { useState } from 'react';
 
-import { ClientConfig, IAgoraRTCClient } from 'agora-rtc-sdk-ng';
+import { RtmClient } from 'agora-rtm-sdk';
 import useAsyncEffect from 'use-async-effect';
+
+import { clientEnv } from '@/env/schema.mjs';
 
 /**
  * Creates a singleton instance of the AgoraRTC client
  * that can be used throughout the app w/ the same hook
  */
-let cachedClient: IAgoraRTCClient;
+let cachedClient: RtmClient;
 
-export const useAgoraRtcClient = (
-  config: ClientConfig = { mode: 'rtc', codec: 'vp8' },
-): { isLoading: boolean; client: IAgoraRTCClient } => {
+export const useAgoraRtmClient = (): {
+  isLoading: boolean;
+  client: RtmClient;
+} => {
   const [isLoading, setLoading] = useState(!cachedClient); // Load when missing client
   const [client, setClient] = useState(cachedClient);
 
   useAsyncEffect(async () => {
     if (client) return;
 
-    const AgoraRTC = (await import('agora-rtc-sdk-ng')).default;
-    const newClient = AgoraRTC.createClient(config);
+    const appId = clientEnv.NEXT_PUBLIC_AGORA_APP_ID;
+    if (!appId) {
+      throw new Error('Missing Agora App ID!');
+    }
+
+    const AgoraRTM = (await import('agora-rtm-sdk')).default;
+    const newClient = AgoraRTM.createInstance(appId);
     cachedClient = newClient;
 
     setClient(newClient);
     setLoading(false);
-  }, [client, config]);
+  }, [client]);
 
   return {
     isLoading,
