34d9bf4c43720981b77101e0b7698f20e81c3afb
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-03-18 15:14:42 +0200

    Fixed selection of the rooms by participants

--- src/server/api/routers/rooms.ts
+++ src/server/api/routers/rooms.ts
@@ -12,7 +12,11 @@ export const roomsRouter = createTRPCRouter({
           {
             sessions: {
               some: {
-                user_id: userId,
+                participants: {
+                  some: {
+                    user_id: userId,
+                  },
+                },
               },
             },
           },
