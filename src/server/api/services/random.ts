35442b8008af4056b8f109e569afbc70cae056c2
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-03-25 15:26:59 +0200

    Excluded `-` from nanoid generation

--- src/server/api/services/random.ts
+++ src/server/api/services/random.ts
@@ -1,4 +1,4 @@
 import { customAlphabet } from 'nanoid';
 
-const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz-';
+const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
 export const shortNanoid = customAlphabet(alphabet, 10);
