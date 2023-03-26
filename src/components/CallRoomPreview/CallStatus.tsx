a297a2ec0814943f7256cdb74c3b0bc4e1610b7a
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-03-26 19:50:47 +0300

    Separated `CalLStatus` component

---
+++ src/components/CallRoomPreview/CallStatus.tsx
@@ -0,0 +1,87 @@
+import React, { FC, ReactElement } from 'react';
+
+import { ClassNames } from '@emotion/react';
+import { Avatar, AvatarGroup, Stack, Typography } from '@mui/material';
+import { uploadcareLoader } from '@uploadcare/nextjs-loader';
+import { first } from 'lodash';
+import Image from 'next/image';
+
+import BlinkingCircle from '@/components/BlinkingCircle';
+import { AVATAR_SIZE, UPLOADCARE_CDN_URL } from '@/constants/index';
+import { lightBackgroundContainer } from '@/styles/mixins';
+import { RouterOutputs } from '@/utils/api';
+
+interface Props {
+  targetRoom: RouterOutputs['rooms']['getRoom'];
+}
+
+const CallStatus: FC<Props> = (props) => {
+  const { targetRoom } = props;
+
+  const lastSession = first(targetRoom!.sessions);
+  const isActiveSession = lastSession?.finishedAt === null;
+
+  const renderInactiveStatus = (): ReactElement => (
+    <Stack direction="row" alignItems="center" gap={1}>
+      <ClassNames>
+        {({ theme }) => (
+          <BlinkingCircle
+            mainColor={theme.palette.success.main}
+            animate={false}
+          />
+        )}
+      </ClassNames>
+      <Typography variant="subtitle1">
+        The call hasn&apos;t started yet
+      </Typography>
+    </Stack>
+  );
+
+  const renderActiveStatus = (): ReactElement => (
+    <>
+      <Stack direction="row" alignItems="center" gap={1}>
+        <BlinkingCircle />
+        <Typography variant="subtitle1">
+          The call is already in progress
+        </Typography>
+      </Stack>
+      <AvatarGroup max={7}>
+        {lastSession!.participants.map(({ user: { id, name, image } }) => (
+          <Avatar key={id}>
+            <Image
+              alt={name!}
+              src={image!}
+              width={AVATAR_SIZE}
+              height={AVATAR_SIZE}
+              quality={80}
+              loader={
+                image!.includes(UPLOADCARE_CDN_URL)
+                  ? uploadcareLoader // Use uploadcore for custom uploaded images
+                  : undefined
+              }
+            />
+          </Avatar>
+        ))}
+      </AvatarGroup>
+    </>
+  );
+
+  return (
+    <Stack
+      css={(theme) => [
+        lightBackgroundContainer(theme, { active: isActiveSession }),
+      ]}
+      alignItems="center"
+      width="80%"
+      gap={1}
+      p={2}
+      borderRadius={6}
+      position="absolute"
+      bottom={100}
+    >
+      {isActiveSession ? renderActiveStatus() : renderInactiveStatus()}
+    </Stack>
+  );
+};
+
+export default CallStatus;
--- src/components/CallRoomPreview/index.tsx
+++ src/components/CallRoomPreview/index.tsx
@@ -1,33 +1,21 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC } from 'react';
 
-import { ClassNames } from '@emotion/react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import LoginIcon from '@mui/icons-material/Login';
 import { LoadingButton } from '@mui/lab';
-import {
-  AvatarGroup,
-  Button,
-  Skeleton,
-  Stack,
-  Tooltip,
-  Typography,
-} from '@mui/material';
-import { uploadcareLoader } from '@uploadcare/nextjs-loader';
-import { first, range } from 'lodash';
-import Image from 'next/image';
+import { Button, Skeleton, Stack, Tooltip, Typography } from '@mui/material';
+import { range } from 'lodash';
 import { useRouter } from 'next/router';
 
-import BlinkingCircle from '@/components/BlinkingCircle';
-import { AVATAR_SIZE, UPLOADCARE_CDN_URL } from '@/constants/index';
+import CallStatus from '@/components/CallRoomPreview/CallStatus';
 import {
   blurBackgroundContainer,
   fullHeight,
   fullWidth,
-  lightBackgroundContainer,
   lineClamp,
   shadowBorder,
 } from '@/styles/mixins';
-import { api, RouterOutputs } from '@/utils/api';
+import { api } from '@/utils/api';
 
 import type { TracksStatus } from '@/pages/preview/[roomId]';
 
@@ -167,76 +155,6 @@ const CallRoomPreview: FC<Props> = (props) => {
           </>
         )}
       </Stack>
-    </Stack>
-  );
-};
-
-const CallStatus: FC<{ targetRoom: RouterOutputs['rooms']['getRoom'] }> = (
-  props,
-) => {
-  const { targetRoom } = props;
-
-  const lastSession = first(targetRoom!.sessions);
-  const isActiveSession = lastSession?.finishedAt === null;
-
-  const renderInactiveStatus = (): ReactElement => (
-    <Stack direction="row" alignItems="center" gap={1}>
-      <ClassNames>
-        {({ theme }) => (
-          <BlinkingCircle
-            mainColor={theme.palette.success.main}
-            animate={false}
-          />
-        )}
-      </ClassNames>
-      <Typography variant="subtitle1">
-        The call hasn&apos;t started yet
-      </Typography>
-    </Stack>
-  );
-
-  const renderActiveStatus = (): ReactElement => (
-    <>
-      <Stack direction="row" alignItems="center" gap={1}>
-        <BlinkingCircle />
-        <Typography variant="subtitle1">
-          The call is already in progress
-        </Typography>
-      </Stack>
-      <AvatarGroup max={7}>
-        {lastSession!.participants.map(({ user: { id, name, image } }) => (
-          <Image
-            key={id}
-            alt={name!}
-            src={image!}
-            width={AVATAR_SIZE}
-            height={AVATAR_SIZE}
-            quality={80}
-            loader={
-              image!.includes(UPLOADCARE_CDN_URL)
-                ? uploadcareLoader // Use uploadcore for custom uploaded images
-                : undefined
-            }
-          />
-        ))}
-      </AvatarGroup>
-    </>
-  );
-
-  return (
-    <Stack
-      css={(theme) => [
-        lightBackgroundContainer(theme, { active: isActiveSession }),
-      ]}
-      alignItems="center"
-      width="80%"
-      gap={1}
-      p={2}
-      borderRadius={6}
-      position="absolute"
-      bottom={100}
-    >
-      {isActiveSession ? renderActiveStatus() : renderInactiveStatus()}
     </Stack>
   );
 };
