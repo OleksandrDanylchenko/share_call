c86210d1259ad54a0a62029a44ae2a2bca6232d0
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-04-04 22:03:45 +0300

    Added delete button for the room

--- src/components/DashboardScene/Rooms/RoomDetails/index.tsx
+++ src/components/DashboardScene/Rooms/RoomDetails/index.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react';
 
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import LoginIcon from '@mui/icons-material/Login';
-import { IconButton, Stack, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+import {
+  bindDialog,
+  bindTrigger,
+  usePopupState,
+} from 'material-ui-popup-state/hooks';
 import { useRouter } from 'next/router';
 
+import { DashboardSceneType } from '@/components/DashboardScene';
 import RoomUpdateForm from '@/components/DashboardScene/Rooms/RoomUpdateForm';
 import PillContainer from '@/components/PillContainer';
+import { shadowBorder } from '@/styles/mixins';
 import { api } from '@/utils/api';
 import RoomInvite from 'components/RoomInvite';
 
@@ -25,12 +46,70 @@ const RoomDetails: FC<Required<Props>> = (props) => {
     <Stack flex={1} gap={4}>
       {activeRoom && (
         <>
+          <RoomDelete activeRoomId={activeRoom.id} />
           <RoomUpdateForm activeRoom={activeRoom} />
           <RoomInvite inviteCode={activeRoom.inviteCode} />
           <RoomJoin activeRoomId={activeRoom.id} />
         </>
       )}
     </Stack>
+  );
+};
+
+const RoomDelete: FC<{ activeRoomId: string }> = (props) => {
+  const { activeRoomId } = props;
+
+  const dialogState = usePopupState({ variant: 'dialog' });
+  const { close } = dialogState;
+
+  const { mutate: deleteRoom, isLoading: deletingRoom } =
+    api.rooms.deleteRoom.useMutation({ onSuccess: close });
+
+  const handleDeleteClick = (): void => {
+    deleteRoom({ id: activeRoomId });
+  };
+
+  return (
+    <>
+      <IconButton
+        css={(theme) =>
+          shadowBorder(theme, {
+            blurRadius: '5px',
+            color: theme.palette.error.main,
+          })
+        }
+        aria-label="Delete a room"
+        sx={{
+          position: 'absolute',
+          right: 20,
+          top: 20,
+        }}
+        {...bindTrigger(dialogState)}
+      >
+        <DeleteOutlineIcon
+          sx={{
+            color: (theme) => theme.palette.error.light,
+          }}
+          fontSize="medium"
+        />
+      </IconButton>
+      <Dialog
+        {...bindDialog(dialogState)}
+        aria-labelledby="room-delete-alert-title"
+      >
+        <DialogTitle id="room-delete-alert-title">
+          Are you sure you want to delete this room?
+        </DialogTitle>
+        <DialogActions>
+          <Button disabled={deletingRoom} onClick={close}>
+            Nope!
+          </Button>
+          <LoadingButton loading={deletingRoom} onClick={handleDeleteClick}>
+            Yes, I&apos;m sure!
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
