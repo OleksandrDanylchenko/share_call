256284b0673a7c05571f8c1e77c6e623da1ffeca
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-04-09 18:24:06 +0300

    Added finite ranges support for the `useDuration`

--- src/hooks/useDuration.ts
+++ src/hooks/useDuration.ts
@@ -2,20 +2,24 @@ import { useEffect, useState } from 'react';
 
 import { DateTime } from 'luxon';
 
-export const useDuration = (start: Date): string => {
+export const useDuration = (start: Date, end?: Date | null): string => {
   const [duration, setDuration] = useState('');
   useEffect(() => {
     const updateDuration = (): void => {
       const startDate = DateTime.fromJSDate(start);
-      const duration = DateTime.local().diff(startDate, ['hours', 'minutes']);
-      setDuration(duration.toFormat('hh:mm'));
+      const endDate = end ? DateTime.fromJSDate(end) : DateTime.local();
+      const duration = endDate.diff(startDate, ['hours', 'minutes', 'seconds']);
+      setDuration(
+        duration.toFormat(duration.minutes > 1 ? 'hh:mm' : 'hh:mm:ss'),
+      );
     };
-
     updateDuration();
-    const minuteInterval = setInterval(updateDuration, 1000);
 
+    if (end) return; // No need for live updates
+
+    const minuteInterval = setInterval(updateDuration, 1000);
     return () => clearInterval(minuteInterval);
-  }, [start]);
+  }, [start, end]);
 
   return duration;
 };
